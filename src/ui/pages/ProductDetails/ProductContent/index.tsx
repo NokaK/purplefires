8d9812d2e09b944af5786d11435d62dcf2d93643
NokaK <nokakipiani93@gmail.com>
2024-04-25 23:37:30 +0400

    rename image names

--- src/ui/pages/ProductDetails/ProductContent/index.tsx
+++ src/ui/pages/ProductDetails/ProductContent/index.tsx
@@ -2,13 +2,7 @@ import {
   ProductDetailsWrapper,
   InfoWrapper,
   ProductContentWrapper,
-  ImagesWrapper,
 } from "./ProductContentStyle";
-import StarIcon from "../../../../assets/details-page/icons/Star.svg";
-import Image from "next/image";
-
-import { useState } from "react";
-import Button from "../../../details-page-components/Button";
 import React from "react";
 import ImagesBlock from "./ImagesBlock";
 import ProductInfoBlock from "./ProductInfoBlock";
