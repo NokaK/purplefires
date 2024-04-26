efc3c01f0d75d77d26d0859000a68cd0465389e9
NokaK <nokakipiani93@gmail.com>
2024-04-26 10:52:51 +0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", images: { unoptimized: true } };
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  experimental: {
+    trailingSlash: false, // or true, depending on your preference
+  },
+};
 
 export default nextConfig;
